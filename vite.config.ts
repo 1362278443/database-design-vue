4bc100346570b33016259765f58ddd59e3346be0
xqy <1362278443@qq.com>
2022-12-10 21:58:25 +0800

    环境配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,53 @@
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { resolve } from 'path'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      reactivityTransform: true,
+    }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+      imports: ['vue'],
+      // eslint 报错解决：'ref' is not defined
+      eslintrc: {
+        // 默认 false, true 启用生成。生成一次就可以，避免每次工程启动都生成，一旦生成配置文件之后，最好把 enable 关掉，即改成 false。
+        // 否则这个文件每次会在重新加载的时候重新生成，这会导致 eslint 有时会找不到这个文件。当需要更新配置文件的时候，再重新打开
+        enabled: false,
+        // filepath: './.eslintrc-auto-import.json', // 默认就是 ./.eslintrc-auto-import.json
+        // globalsPropValue: true, // 默认 true
+      },
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  // 别名设置
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'), // 把 @ 指向到 src 目录去
+    },
+  },
+  // 服务设置
+  server: {
+    host: true, // host设置为true才可以使用network的形式，以ip访问项目
+    port: 8080, // 端口号
+    open: true, // 自动打开浏览器
+    cors: true, // 跨域设置允许
+    strictPort: true, // 如果端口已占用直接退出
+    // 接口代理
+    proxy: {
+      '/api': {
+        // 本地 8000 前端代码的接口 代理到 8888 的服务端口
+        target: 'http://localhost:8888/',
+        changeOrigin: true, // 允许跨域
+        rewrite: (path) => path.replace('/api/', '/'),
+      },
+    },
+  },
 })
